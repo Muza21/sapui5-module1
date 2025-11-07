d86134230ed11b7424e730273231b1faf167e548
Muza21 <lasha.muzashvili1@gmail.com>
2025-11-07 12:59:30 +0400

    step10: descriptor for applications

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,24 +1,17 @@
 sap.ui.define(
-  [
-    "sap/ui/core/UIComponent",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel",
-  ],
-  (UIComponent, JSONModel, ResourceModel) => {
+  ["sap/ui/core/UIComponent", "sap/ui/model/json/JSONModel"],
+  (UIComponent, JSONModel) => {
     "use strict";
 
     return UIComponent.extend("ui5.walkthrough.Component", {
       metadata: {
         interfaces: ["sap.ui.core.IAsyncContentCreation"],
-        rootView: {
-          viewName: "ui5.walkthrough.view.App",
-          type: "XML",
-          id: "app",
-        },
+        manifest: "json",
       },
 
       init() {
         UIComponent.prototype.init.apply(this, arguments);
+
         const oData = {
           recipient: {
             name: "World",
@@ -26,11 +19,6 @@ sap.ui.define(
         };
         const oModel = new JSONModel(oData);
         this.setModel(oModel);
-
-        const i18nModel = new ResourceModel({
-          bundleName: "ui5.walkthrough.i18n.i18n",
-        });
-        this.setModel(i18nModel, "i18n");
       },
     });
   }
