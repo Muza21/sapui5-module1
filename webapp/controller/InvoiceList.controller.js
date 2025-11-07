d147cec897875c20cab36025e9c8e4286fe2ddc0
Muza21 <lasha.muzashvili1@gmail.com>
2025-11-07 17:50:24 +0400

    step30: routing and navigation

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -29,6 +29,11 @@ sap.ui.define(
         const oBinding = oList.getBinding("items");
         oBinding.filter(aFilter);
       },
+
+      onPress() {
+        const oRouter = this.getOwnerComponent().getRouter();
+        oRouter.navTo("detail");
+      },
     });
   }
 );
