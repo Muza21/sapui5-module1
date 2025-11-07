987e28b18a7c0e4d214b25b1e1e4abd12ece38b5
Muza21 <lasha.muzashvili1@gmail.com>
2025-11-07 19:39:37 +0400

    feat: filter invoices by status

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -14,6 +14,18 @@ sap.ui.define(
           currency: "EUR",
         });
         this.getView().setModel(oViewModel, "view");
+
+        const oStatus = {
+          statuses: [
+            { key: "ALL", text: "All" },
+            { key: "A", text: "New" },
+            { key: "B", text: "In Progress" },
+            { key: "C", text: "Done" },
+          ],
+          defaultKey: "ALL",
+        };
+        const oStatusModel = new JSONModel(oStatus);
+        this.getView().setModel(oStatusModel, "status");
       },
 
       onFilterInvoices(oEvent) {
@@ -27,6 +39,19 @@ sap.ui.define(
 
         const oList = this.byId("invoiceList");
         const oBinding = oList.getBinding("items");
+        oBinding.filter(aFilter);
+      },
+
+      onFilterInvoicesByStatus(oEvent) {
+        const sSelectedKey = oEvent.getParameter("selectedItem").getKey();
+        const oList = this.byId("invoiceList");
+        const oBinding = oList.getBinding("items");
+        const aFilter = [];
+
+        if (sSelectedKey !== "ALL") {
+          aFilter.push(new Filter("Status", FilterOperator.EQ, sSelectedKey));
+        }
+
         oBinding.filter(aFilter);
       },
 
